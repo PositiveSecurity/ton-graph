bd3e2150bfee143831312c0b369b7d7a55cc8e8c
Viktor Ryabinin <vryabinin@ptsecurity.com>
2025-04-10 11:04:54 +0300

    rebuild script updated

--- rebuild.sh
+++ rebuild.sh
@@ -3,39 +3,22 @@
 rm -rf ./package-lock.json
 rm -rf ./node_modules
 rm -rf ./vsix
+rm -rf ./out
 
+# Install dependencies
 npm install
+
+# Install vsce locally
+npm install -D @vscode/vsce
 
 # Compile the project
 npm run compile
 
-# Create directory for VSIX
-mkdir -p ./vsix/extension
+# Create cached directory if it doesn't exist
+mkdir -p ./cached
 
-# Copy necessary files
-cp -r ./out ./vsix/extension/
-cp -r ./pic ./vsix/extension/
-cp package.json README.md LICENSE ./vsix/extension/
+# Create pic directory if it doesn't exist
+mkdir -p ./pic
 
-# Create cached directory for Mermaid library
-mkdir -p ./vsix/extension/cached
-
-# Go to vsix directory
-cd ./vsix
-
-# Create manifest with updated version
-echo '{
-  "id": "ton-graph",
-  "version": "0.2.1",
-  "type": "Microsoft.VisualStudio.Code.Manifest",
-  "manifestVersion": 1,
-  "publisher": "positiveweb3",
-  "engines": {
-    "vscode": "^1.60.0"
-  }
-}' > extension.vsixmanifest
-
-# Create VSIX file with new version
-zip -r ../ton-graph-0.2.1.vsix extension extension.vsixmanifest
-
-cd ../
+# Package the extension using local vsce
+npx vsce package --allow-unused-files-pattern

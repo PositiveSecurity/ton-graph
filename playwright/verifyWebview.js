6f51e29b7fffc4ef131da50965e22b16d7f880c9
Ilya Druzhinin <fbshop@yandex.ru>
2025-06-05 20:15:23 +0300

    Add Playwright tests and CI

---
+++ playwright/verifyWebview.js
@@ -0,0 +1,31 @@
+const vscode = require('vscode');
+const path = require('path');
+
+module.exports = async function () {
+  const ext = vscode.extensions.getExtension('positiveweb3.ton-graph');
+  if (ext) {
+    await ext.activate();
+  }
+
+  const panelStub = {
+    webview: {
+      html: '',
+      asWebviewUri: (uri) => uri,
+      onDidReceiveMessage: () => {},
+      postMessage: () => Promise.resolve(true)
+    },
+    onDidDispose: () => {}
+  };
+
+  const originalCreate = vscode.window.createWebviewPanel;
+  vscode.window.createWebviewPanel = () => panelStub;
+
+  const samplePath = path.join(__dirname, '..', 'examples', 'func_nominators.fc');
+  await vscode.commands.executeCommand('ton-graph.visualize', vscode.Uri.file(samplePath));
+
+  if (!panelStub.webview.html.includes('.mermaid')) {
+    throw new Error('Mermaid script not found in webview');
+  }
+
+  vscode.window.createWebviewPanel = originalCreate;
+};

0095abbb3e7326b9f9028cfd8ae7aa99dd249ba0
Ilya Druzhinin <fbshop@yandex.ru>
2025-06-06 08:22:30 +0300

    test: cover error paths

---
+++ test/parserUtilsInvalid.test.ts
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+import mock = require('mock-require');
+
+mock('vscode', { window: { createOutputChannel: () => ({ appendLine: () => {} }) } });
+
+describe('parserUtils invalid inputs', () => {
+  afterEach(() => {
+    mock.stop('../src/parser/funcParser');
+    delete require.cache[require.resolve('../src/parser/parserUtils')];
+  });
+
+  it('falls back to FunC parser for unknown language', async () => {
+    delete require.cache[require.resolve('../src/parser/parserUtils')];
+    let called = false;
+    mock('../src/parser/funcParser', { parseContractCode: async () => { called = true; return { nodes: [], edges: [] }; } });
+    const parserUtils = require('../src/parser/parserUtils');
+    const res = await parserUtils.parseContractByLanguage('code', 'weird' as any);
+    expect(called).to.be.true;
+    expect(res).to.deep.equal({ nodes: [], edges: [] });
+  });
+
+  it('detects unknown extension as func', () => {
+    delete require.cache[require.resolve('../src/parser/parserUtils')];
+    const parserUtils = require('../src/parser/parserUtils');
+    expect(parserUtils.detectLanguage('file.unknown')).to.equal('func');
+  });
+});

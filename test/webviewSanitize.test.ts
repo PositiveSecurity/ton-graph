a8465958b9b3610c8f4393483bf91de37cab32eb
Ilya Druzhinin <fbshop@yandex.ru>
2025-06-06 21:52:45 +0300

    harden webview links

--- test/webviewSanitize.test.ts
+++ test/webviewSanitize.test.ts
@@ -21,6 +21,71 @@ describe('webview sanitize', () => {
     expect(clean).to.not.include('onload');
   });
 
+  it('neutralizes javascript links', async () => {
+    const html = `<!DOCTYPE html>
+      <div id="mermaid-diagram"></div>`;
+    const dom = new JSDOM(html, { url: 'http://localhost' });
+    (global as any).window = dom.window as any;
+    (global as any).document = dom.window.document;
+    (global as any).filterSet = [];
+    (global as any).acquireVsCodeApi = () => ({ postMessage: () => {} });
+    (global as any).mermaid = {
+      initialize: () => {},
+      render: () => Promise.resolve({ svg: '<svg><a href="javascript:alert(1)">bad</a></svg>' })
+    };
+
+    mock('dompurify', { default: { sanitize: (s: string) => s } });
+    delete require.cache[require.resolve('../src/webview/index.ts')];
+    require('../src/webview/index.ts');
+
+    await new Promise(res => setTimeout(res, 0));
+
+    const link = dom.window.document.querySelector('a')!;
+    expect(link.hasAttribute('href')).to.be.false;
+
+    mock.stopAll();
+    delete require.cache[require.resolve('../src/webview/index.ts')];
+    dom.window.close();
+    delete (global as any).window;
+    delete (global as any).document;
+    delete (global as any).filterSet;
+    delete (global as any).acquireVsCodeApi;
+    delete (global as any).mermaid;
+  });
+
+  it('enforces rel and target on external links', async () => {
+    const html = `<!DOCTYPE html>
+      <div id="mermaid-diagram"></div>`;
+    const dom = new JSDOM(html, { url: 'http://localhost' });
+    (global as any).window = dom.window as any;
+    (global as any).document = dom.window.document;
+    (global as any).filterSet = [];
+    (global as any).acquireVsCodeApi = () => ({ postMessage: () => {} });
+    (global as any).mermaid = {
+      initialize: () => {},
+      render: () => Promise.resolve({ svg: '<svg><a href="http://example.com">ok</a></svg>' })
+    };
+
+    mock('dompurify', { default: { sanitize: (s: string) => s } });
+    delete require.cache[require.resolve('../src/webview/index.ts')];
+    require('../src/webview/index.ts');
+
+    await new Promise(res => setTimeout(res, 0));
+
+    const link = dom.window.document.querySelector('a')!;
+    expect(link.getAttribute('target')).to.equal('_blank');
+    expect(link.getAttribute('rel')).to.equal('noopener noreferrer');
+
+    mock.stopAll();
+    delete require.cache[require.resolve('../src/webview/index.ts')];
+    dom.window.close();
+    delete (global as any).window;
+    delete (global as any).document;
+    delete (global as any).filterSet;
+    delete (global as any).acquireVsCodeApi;
+    delete (global as any).mermaid;
+  });
+
   it('ignores malicious message events', async () => {
     const html = `<!DOCTYPE html>
       <div id="mermaid-diagram"></div>`;
